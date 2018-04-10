78defe1a4619cae1a7fb8cd4d2988b4252c2a097
Ziyi-Guo <kwokjona@gmail.com>
2018-04-10 21:46:54 +0800

    Set loss as “Gen_loss - alpha * hinge_loss” where hinge_loss is the Disc loss in last GAN(SVM)

--- train.py
+++ train.py
@@ -3,32 +3,34 @@ import dataset
 from TI_GAN import *
 import numpy as np
 from sklearn import svm
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 # Hyper Parameters
-sample_amount = 300
+sample_amount = 200
 # batch_size matters when sample_amount is rather small
 batch_size = 8
 learning_rate = 2e-4
-training_step = 1000*10
+training_step = 1000*8
 display_step = 100
+alpha = 0.4
 
 # Network Parameters
 image_dim = 784
 noise_dim = 64
-desired_class = 9
+desired_class = [8, 9]
 
 # Data Feed
 # 784 (reshape=True) | 28*28 (reshape=False)
 image_reshape = False
 data_dir = "/home/ziyi/code/data/"
-mnist0,mnist1 = dataset.read_data_sets(data_dir, target_class=desired_class, one_hot=False,
+mnist0 = dataset.read_data_sets(data_dir, target_class=desired_class[0], one_hot=False,
                                 reshape=image_reshape, sample_vol=sample_amount)
 
-print(mnist1.train.images.shape, mnist1.train.num_examples)
+# print(mnist1.train.images.shape, mnist1.train.num_examples)
 
-# mnist1 = dataset.read_data_sets(data_dir, target_class=desired_class[1], one_hot=False,
-#                                 reshape=image_reshape, sample_vol=sample_amount)
+mnist1 = dataset.read_data_sets(data_dir, target_class=desired_class[1], one_hot=False,
+                                reshape=image_reshape, sample_vol=sample_amount*10)
 
 # Graph Input
 gen_input = tf.placeholder(tf.float32, [None, noise_dim])
@@ -37,29 +39,37 @@ real_image_input1 = tf.placeholder(tf.float32, [None, 28, 28, 1])
 # Targets Input
 disc_target = tf.placeholder(tf.int32, shape=[None])
 gen_target = tf.placeholder(tf.int32, shape=[None])
-
-disc_target_all = tf.placeholder(tf.int32, shape=[None])
+disc_target_real = tf.placeholder(tf.int32, shape=[None])
 disc_target_gen = tf.placeholder(tf.int32, shape=[None])
 
 # All operations defined on variables
-gen_sample0, gen_train0, disc_train0, gen_loss_op0, disc_loss_op0 = \
+gen_sample0, gen0_loss, disc0_loss = \
     train_operations(gen_input, real_image_input0, disc_target, gen_target, index="0")
 
-gen_sample1, gen_train1, disc_train1, gen_loss_op1, disc_loss_op1 = \
+gen_sample1, gen1_loss, disc1_loss = \
     train_operations(gen_input, real_image_input1, disc_target, gen_target, index="1")
 
-cross_gen_loss, cross_disc_loss, cross_gen_train, cross_disc_train = \
+cross_gen_loss, cross_disc_loss = \
     cross_class_operations(gen_sample0, gen_sample1, real_image_input0,
-                           real_image_input1, disc_target_all, disc_target_gen)
+                           real_image_input1, disc_target_real, disc_target_gen)
 
-clf = svm.SVC(kernel='linear')
-# mlp_prev = mlp(real_image_input0, scoop_name="Prev")
-# pred = tf.nn.softmax(mlp_prev)
-# # mlp_after = mlp(tf.concat([real_image_input0, real_image_input1], axis=0), scoop_name="After")
-# mlp_loss = tf.reduce_mean(
-#     tf.nn.sparse_softmax_cross_entropy_with_logits(labels=disc_target, logits=mlp_prev))
-# mlp_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Prev")
-# mlp_train = tf.train.AdamOptimizer(learning_rate).minimize(mlp_loss,var_list=mlp_vars)
+
+gen0_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Generator0")
+gen1_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Generator1")
+disc0_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator0")
+disc1_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator1")
+cross_disc_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Cross_Discriminator")
+
+gen0_loss = tf.subtract(gen0_loss, alpha * cross_gen_loss)
+gen1_loss = tf.subtract(gen1_loss, alpha * cross_gen_loss)
+# Optimizer Definition
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+# Trainer in minimizing loss
+gen0_train = optimizer.minimize(gen0_loss, var_list=gen0_vars)
+gen1_train = optimizer.minimize(gen1_loss, var_list=gen1_vars)
+disc0_train = optimizer.minimize(disc0_loss, var_list=disc0_vars)
+disc1_train = optimizer.minimize(disc1_loss, var_list=disc1_vars)
+disc_cross_train = optimizer.minimize(cross_disc_loss, var_list=cross_disc_vars)
 
 
 merged = tf.summary.merge_all()
@@ -72,24 +82,14 @@ with tf.Session(config=config) as sess:
     sess.run(init)
 
     train_images = np.concatenate([mnist0.train.images, mnist1.train.images], 0)
-    train_labels = np.concatenate([np.ones(mnist0.train.num_examples), np.zeros(mnist1.train.num_examples)])
+    train_labels = np.concatenate([np.ones(mnist0.train.num_examples), -np.ones(mnist1.train.num_examples)])
     test_num = mnist0.test.num_examples
     test_images = np.concatenate([mnist0.test.images, mnist1.test.images], 0)
-    test_labels = np.concatenate([np.ones(test_num), np.zeros(mnist1.test.num_examples)])
-    clf.fit(train_images.reshape([-1, 28*28]), train_labels)
-    prediction = clf.predict(test_images.reshape([-1, 28*28]))
+    test_labels = np.concatenate([np.ones(test_num), -np.ones(mnist1.test.num_examples)])
+    clf0 = svm.SVC(kernel='linear')
+    clf0.fit(train_images.reshape([-1, 28*28]), train_labels)
+    prediction = clf0.predict(test_images.reshape([-1, 28*28]))
     print(np.mean(prediction==test_labels)*1.)
-    # for idx in range(500):
-    #     batch_x0, _ = mnist0.train.next_batch(batch_size)
-    #     batch_x1, _ = mnist1.train.next_batch(batch_size)
-    #     batch_disc_y = np.concatenate([np.ones([batch_size]), np.zeros([batch_size])], axis=0)
-    #     m_loss, _ = sess.run([mlp_loss, mlp_train],
-    #                          feed_dict={real_image_input0: np.concatenate([batch_x0,batch_x1],0),disc_target:batch_disc_y})
-    #
-    # pred_correct = tf.equal(tf.argmax(pred, 1, output_type=tf.int32), disc_target)
-    # accuracy = tf.reduce_mean(tf.cast(pred_correct, tf.float32))
-    # print(sess.run([mlp_loss, accuracy],
-    #                feed_dict={real_image_input0:test_images, disc_target: test_labels}))
 
     for idx in range(training_step):
         # Sample data for Disc and Gen
@@ -103,20 +103,21 @@ with tf.Session(config=config) as sess:
         # Sample labels for Disc
         batch_gen_y = np.ones([batch_size])
         batch_disc_y = np.concatenate([np.ones([batch_size]), np.zeros([batch_size])], axis=0)
-        batch_disc_gen = np.concatenate([np.ones([batch_size]), np.zeros([batch_size])], axis=0)
-        batch_disc_all = np.concatenate([np.zeros([batch_size*2]), np.ones([batch_size*2])], axis=0)
+        batch_disc_gen = np.concatenate([np.ones([batch_size]), -np.ones([batch_size])], axis=0)
+        batch_disc_real = np.concatenate([np.ones([batch_size]), -np.ones([batch_size])], axis=0)
 
         feed_dict = {
             gen_input: z, real_image_input0: batch_x0, real_image_input1: batch_x1,
             disc_target: batch_disc_y, gen_target: batch_gen_y,
-            disc_target_all: batch_disc_all, disc_target_gen: batch_disc_gen
+            disc_target_real: batch_disc_real, disc_target_gen: batch_disc_gen
                      }
         ops = [
-            merged, gen_train0, disc_train0, gen_loss_op0, disc_loss_op0,
-            gen_train1, disc_train1, gen_loss_op1, disc_loss_op1,
-            cross_gen_loss, cross_disc_loss, cross_gen_train, cross_disc_train
+            merged, gen0_train, disc0_train, gen0_loss, disc0_loss,
+            gen1_train, disc1_train, gen1_loss, disc1_loss,
+            cross_gen_loss, cross_disc_loss
         ]
-        summary, _, _, gl0, dl0, _, _, gl1, dl1, cgl, cdl, _, _ = sess.run(ops, feed_dict=feed_dict)
+        summary, _, _, gl0, dl0, _, _, gl1, dl1, cgl, cdl = sess.run(ops, feed_dict=feed_dict)
+        # print(cdl)
 
         if (idx + 1) % display_step == 0:
             history_writer.add_summary(summary, idx)
@@ -125,33 +126,20 @@ with tf.Session(config=config) as sess:
             if (idx + 1) >= 4000:
                 g0, g1 = sess.run([gen_sample0, gen_sample1], feed_dict={gen_input: z})
                 mnist0.train.concat_batch(g0)
-                # mnist1.train.concat_batch(g1)
+                mnist1.train.concat_batch(g1)
         if (idx+1) % 1000 == 0:
             d = int((idx+1)/1000)
             plot_image(sess, gen_sample0, gen_sample1, noise_dim, desired_class, sample_amount, gen_input, d)
 
-    # for idx in range(500):
-    #     batch_x0, _ = mnist0.train.next_batch(batch_size)
-    #     batch_x1, _ = mnist1.train.next_batch(batch_size)
-    #     batch_disc_y = np.concatenate([np.ones([batch_size]), np.zeros([batch_size])], axis=0)
-    #     m_loss, _ = sess.run([mlp_loss, mlp_train],
-    #                          feed_dict={real_image_input0: np.concatenate([batch_x0,batch_x1],0),disc_target:batch_disc_y})
-
-    # pred_correct = tf.equal(tf.argmax(pred, 1, output_type=tf.int32), disc_target)
-    # accuracy = tf.reduce_mean(tf.cast(pred_correct, tf.float32))
-    # test_num = mnist0.test.num_examples
-    # test_images = np.concatenate([mnist0.test.images, mnist1.test.images], 0)
-    # test_labels = np.concatenate([np.ones(test_num),np.zeros(mnist1.test.num_examples)])
-    # print(sess.run([mlp_loss, accuracy],
-    #                feed_dict={real_image_input0:test_images, disc_target: test_labels}))
-
+    print(mnist0.train.num_examples)
     train_images = np.concatenate([mnist0.train.images, mnist1.train.images], 0)
-    train_labels = np.concatenate([np.ones(mnist0.train.num_examples), np.zeros(mnist1.train.num_examples)])
+    train_labels = np.concatenate([np.ones(mnist0.train.num_examples), -np.ones(mnist1.train.num_examples)])
     test_num = mnist0.test.num_examples
     test_images = np.concatenate([mnist0.test.images, mnist1.test.images], 0)
-    test_labels = np.concatenate([np.ones(test_num), np.zeros(mnist1.test.num_examples)])
-    clf.fit(train_images.reshape([-1, 28 * 28]), train_labels)
-    prediction = clf.predict(test_images.reshape([-1, 28 * 28]))
+    test_labels = np.concatenate([np.ones(test_num), -np.ones(mnist1.test.num_examples)])
+    clf1 = svm.SVC(kernel='linear')
+    clf1.fit(train_images.reshape([-1, 28 * 28]), train_labels)
+    prediction = clf1.predict(test_images.reshape([-1, 28 * 28]))
     print(np.mean(prediction == test_labels) * 1.)
 
     history_writer.close()
